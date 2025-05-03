32282c1f133d579c8a075573147e75bdf77ef0e2
BirdCatcherGames <birdcatchergames@gmail.com>
2025-05-02 20:00:49 -0700

    Adding missing source file
    Having automated builds would be great. A man can dream.

---
+++ Source/BCICore/Private/AssetDataUtilities.cpp
@@ -0,0 +1,18 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+
+#include "AssetDataUtilities.h"
+
+#include "BCICoreLogCategory.h"
+#include "Macros/Log.h"
+
+void AssetDataUtilities::Log(const FAssetData& AssetData, const bool bDeserialized)
+{
+	if (bDeserialized)
+	{
+		BCILOG(LogBCICore, Verbose, "Got asset data", {"assetData", AssetData.GetPrimaryAssetId() }, {"deserialized", true });
+	}
+	else
+	{
+		BCILOG(LogBCICore, Warning, "Unable to serialize asset", {"assetData", AssetData.GetPrimaryAssetId() }, {"deserialized", false });
+	}	
+}
